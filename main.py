436051268c1bbffd73415beef12426a20ad519ce
AniShalev <37079378+AniShalev@users.noreply.github.com>
2020-02-23 23:37:17 +0200

    Update main.py

--- main.py
+++ main.py
@@ -28,11 +28,11 @@ def tweet(status):
 	config.read('settings.cfg')
 	
 	# http://dev.twitter.com/apps/myappid
-	CONSUMER_KEY = config.get('API Information', 'CONSUMER_KEY')
-	CONSUMER_SECRET = config.get('API Information', 'CONSUMER_SECRET')
+	CONSUMER_KEY = os.environ.get('TWITTER_ACCESS_TOKEN', None)
+	CONSUMER_SECRET = os.environ.get('TWITTER_ACCESS_TOKEN', None)
 	# http://dev.twitter.com/apps/myappid/my_token
-	ACCESS_TOKEN_KEY = config.get('API Information', 'ACCESS_TOKEN_KEY')
-	ACCESS_TOKEN_SECRET = config.get('API Information', 'ACCESS_TOKEN_SECRET')
+	ACCESS_TOKEN_KEY = os.environ.get('TWITTER_ACCESS_TOKEN', None)
+	ACCESS_TOKEN_SECRET = os.environ.get('TWITTER_ACCESS_TOKEN', None)
 
 	auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 	auth.set_access_token(ACCESS_TOKEN_KEY, ACCESS_TOKEN_SECRET)
@@ -44,7 +44,7 @@ def get_current_line(index):
 	with open("words.txt") as source_fh:
 		for i in range(index+1):
 			status_str = source_fh.readline().strip()
-		return "fuck " + status_str
+		return "זין על " + status_str
 
 class MainHandler(webapp2.RequestHandler):
 	def get(self):
@@ -54,7 +54,7 @@ class MainHandler(webapp2.RequestHandler):
 		if users.get_current_user():
 			url = users.create_logout_url(self.request.uri)
 			url_linktext = 'Hello, ' + user.nickname() + '. Logout'
-			if user.nickname() == "anka.213" or user.nickname() == "test@example.com":
+			if user.nickname() == "anishalev1" or user.nickname() == "test@example.com":
 				disabled = ""
 			else:
 				disabled = "disabled"
@@ -108,7 +108,7 @@ class MakeHandler(webapp2.RequestHandler):
 
 class TestHandler(webapp2.RequestHandler):
 	def get(self):
-		tweet('fuck test')
+		tweet('זין על ניסיון')
 
 		self.redirect('/')
 
